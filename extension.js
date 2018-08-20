c28abdb49b35ac855c2c23c4b482d3846e3f4af0
Dominik Kadera <dominik.kadera@integromat.com>
2018-08-20 12:25:01 +0200

    Core commands update

--- extension.js
+++ extension.js
@@ -1,8 +1,6 @@
 const vscode = require('vscode')
 
 const AppsProvider = require('./src/providers/AppsProvider')
-const ImlProvider = require('./src/providers/ImlProvider')
-const RpcProvider = require('./src/providers/RpcProvider')
 const KeywordProvider = require('./src/providers/KeywordProvider')
 const OpensourceProvider = require('./src/providers/OpensourceProvider')
 const ImljsonHoverProvider = require('./src/providers/ImljsonHoverProvider')
@@ -22,12 +20,8 @@ const ChangesCommands = require('./src/commands/ChangesCommands')
 const AccountCommands = require('./src/commands/AccountCommands')
 const CoreCommands = require('./src/commands/CoreCommands')
 
-const request = require('request')
 const tempy = require('tempy')
-const fs = require('fs')
 const path = require('path')
-const mkdirp = require('mkdirp')
-const rp = require('request-promise')
 
 var _configuration
 var _authorization
@@ -41,7 +35,6 @@ async function activate() {
     _configuration = vscode.workspace.getConfiguration('apps-sdk')
     console.log('Apps SDK active.');
 
-    //#region ENV COMMANDS AND HANDLING
     /**
      * Add new environment
      */
@@ -149,7 +142,6 @@ async function activate() {
             vscode.commands.executeCommand('apps-sdk.env.add')
         }
     }
-    //#endregion
 
     if(_configuration.environment != ""){
         
@@ -186,334 +178,11 @@ async function activate() {
         let appsProvider = new AppsProvider(_authorization, _environment);
         vscode.window.registerTreeDataProvider('apps', appsProvider);
 
-        let coreCommands = new CoreCommands(appsProvider, _authorization, _environment)
-
-        //#region CORE COMMANDS
         /**
-         * SOURCE-LOADER
-         * This block is responsible for loading source codes from Integromat
+         * Registering commands
          */
-        vscode.commands.registerCommand('apps-sdk.load-source', async function (item) {            
-
-            // Compose directory structure
-            let urn = `/app/${Core.getApp(item).name}`
-
-            // Add version to URN for versionable items
-            if (Core.isVersionable(item.apiPath)) {
-                urn += `/${Core.getApp(item).version}`
-            }
-
-            // Complete the URN by the type of item
-            switch (item.apiPath) {
-                case "function":
-                    urn += `/${item.apiPath}/${item.parent.name}/code`
-                    break
-                case "rpc":
-                case "module":
-                case "connection":
-                case "webhook":
-                    urn += `/${item.apiPath}/${item.parent.name}/${item.name}`
-                    break
-                case "app":
-                    // Prepared for more app-level codes
-                    switch(item.name){
-                        case "content":
-                            urn += `/docs`
-                            break
-                        default:
-                            urn += `/${item.name}`
-                            break;
-                    }
-                    break
-            }
-
-            // Prepare filepath and dirname for the code
-            let filepath = `${urn}.${item.language}`
-            let dirname = path.dirname(filepath)
-
-
-            /**
-             * CODE-LOADER
-             * Code loader loads the requested code
-             */
-
-            // Prepare the directory for the code 
-            mkdirp(path.join(_DIR, dirname), function (err) {
-
-                // If something went wrong, throw an error and return
-                if (err) {
-                    vscode.window.showErrorMessage(err.message)
-                    return
-                }
-                
-                // Compose a download URL
-                let url = _environment + urn
-
-                /**
-                 * GET THE SOURCE CODE
-                 * Those lines are responsible straight for the download of code
-                 */
-                request({
-                    url: url,
-                    headers: {
-                        'Authorization': _authorization
-                    }
-                }, function (error, response, body) {
-
-                    // Prepare a stream to be saved
-                    let write = (item.language === "js" || item.language === "md") ? body : JSON.stringify(JSON.parse(body), null, 4)
-
-                    // Save the received code to the temp directory
-                    fs.writeFile(path.join(_DIR, filepath), write, function (err) {
-
-                        // If there was an error, display it and return
-                        if (err) {
-                            return vscode.window.showErrorMessage(err.message);
-                        }
-
-                        // Open the downloaded code in the editor
-                        vscode.window.showTextDocument(vscode.workspace.openTextDocument(path.join(_DIR, filepath)), {
-                            preview: false
-                        })
-                    });
-                });
-                
-            })
-        })
-
-        /**
-         * OPEN-SOURCE-LOADER
-         * This block is responsible for loading open source codes from Integromat
-         */
-        vscode.commands.registerCommand('apps-sdk.load-open-source', async function (item) {
-
-            // Get rid of all active providers - no need for them in read-only app
-            if (currentRpcProvider !== null && currentRpcProvider !== undefined) {
-                currentRpcProvider.dispose()
-            }
-            if (currentImlProvider !== null && currentImlProvider !== undefined) {
-                currentImlProvider.dispose()
-            }
-
-            // Compose directory structure
-            let urn = `/app/${Core.getApp(item).name}`
-
-            // Add version to URN for versionable items
-            if (Core.isVersionable(item.apiPath)) {
-                urn += `/${Core.getApp(item).version}`
-            }
-
-            // Complete the URN by the type of item
-            switch (item.apiPath) {
-                case "function":
-                    urn += `/${item.apiPath}/${item.parent.name}/code`
-                    break
-                case "rpc":
-                case "module":
-                case "connection":
-                case "webhook":
-                    urn += `/${item.apiPath}/${item.parent.name}/${item.name}`
-                    break
-                case "app":
-                    // Prepared for more app-level codes
-                    switch(item.name){
-                        case "content":
-                            urn += `/docs`
-                            break
-                        default:
-                            urn += `/${item.name}`
-                            break;
-                    }
-                    break
-            }
-
-            // Prepare filepath and dirname for the code
-            let filepath = `${urn}.${item.language}`
-            let dirname = path.dirname(filepath)
-
-            /**
-             * CODE-LOADER
-             * Code loader loads the requested code
-             */
-
-            // Prepare the directory for the code 
-            mkdirp(path.join(_DIR, "opensource", dirname), function (err) {
-
-                // If something went wrong, throw an error and return
-                if (err) {
-                    vscode.window.showErrorMessage(err.message)
-                    return
-                }
-                
-                // Compose a download URL
-                let url = _environment + urn
-
-                /**
-                 * GET THE SOURCE CODE
-                 * Those lines are responsible straight for the download of code
-                 */
-                request({
-                    url: url,
-                    headers: {
-                        'Authorization': _authorization
-                    }
-                }, function (error, response, body) {
-
-                    // Prepare a stream to be saved
-                    let write = (item.language === "js" || item.language === "md") ? body : JSON.stringify(JSON.parse(body), null, 4)
-
-                    // Save the received code to the temp directory
-                    fs.writeFile(path.join(_DIR, "opensource", filepath), write, { mode: 440 }, function (err) {
-
-                        // If there was an error, display it and return
-                        if (err) {
-                            return vscode.window.showErrorMessage(err.message);
-                        }
-
-                        // Open the downloaded code in the editor
-                        vscode.window.showTextDocument(vscode.workspace.openTextDocument(path.join(_DIR, "opensource", filepath)), {
-                            preview: false
-                        })
-                    });
-                });
-                
-            })
-        })
-
-        /**
-         * Source Uploader
-         */
-        vscode.workspace.onDidSaveTextDocument(editor => coreCommands.sourceUpload(editor))
-
-        /**
-         * RPC-IML PROVIDER KEEPER
-         * Keeps providers up to date
-         */
-        vscode.window.onDidChangeActiveTextEditor(async(editor) => {
-
-            //If undefined, don't do anything
-            if(!editor){ return }
-
-            // Get the URN path of files URI (the right path)
-            let right = editor.document.fileName.split("apps-sdk")[1]
-
-            // No right, no apps-sdk, don't do anything
-            if(!right){ return }
-
-            // If on DOS-base, replace backslashes with forward slashes (on Unix-base it's done already)
-            right = right.replace(/\\/g, "/")
-
-            // Prepare data for loaders
-            let crumbs = right.split("/")
-
-            // If versionable, stash the version
-            let version
-            if(!isNaN(crumbs[3])){
-                version = crumbs[3]
-                crumbs.splice(3,1)
-            }
-
-            // If no version, set 1
-            version = version === undefined ? 1 : version;
-
-            // If not enough crumbs (=> base/docs) -> remove existing providers and exit.
-            if(crumbs.length < 5){ 
-
-                // Remove existing RpcProvider
-                if (currentRpcProvider !== null && currentRpcProvider !== undefined) {
-                    currentRpcProvider.dispose()
-                }
-
-                // Remove existing ImlProvider
-                if (currentImlProvider !== null && currentImlProvider !== undefined) {
-                    currentImlProvider.dispose()
-                }
-                
-                return
-            }
-
-            let apiPath = crumbs[3]
-            let name = crumbs[5].split(".")[0]
-            let app = crumbs[2]
-            
-
-            /**
-             * RPC-LOADER
-             * RpcLoader loads all RPCs available within the app
-             * Following condition specifies where are RPCs allowed
-             */
-            if (
-                (apiPath === "connection" && name === "parameters") ||
-                (apiPath === "webhook" && name === "parameters") ||
-                (apiPath === "module" && (name === "parameters" || name === "expect" || name === "interface" || name === "samples")) ||
-                (apiPath === "rpc" && name === "api")
-            ){
-                // Remove existing RpcProvider
-                if (currentRpcProvider !== null && currentRpcProvider !== undefined) {
-                    currentRpcProvider.dispose()
-                }
-
-                // RPC list url
-                let url = `${_environment}/app/${app}/${version}/rpc`
-
-                // Get list of RPCs
-                let response = await Core.rpGet(url, _authorization)
-                let rpcs = response.map(rpc => {
-                    return `rpc://${rpc.name}`
-                })
-
-                // Register a new RpcProvider
-                currentRpcProvider = vscode.languages.registerCompletionItemProvider({
-                    scheme: 'file',
-                    language: 'imljson'
-                }, new RpcProvider(rpcs))
-            }
-            else {
-
-                // If out of scope, remove existing RpcProvider
-                if (currentRpcProvider !== null && currentRpcProvider !== undefined) {
-                    currentRpcProvider.dispose()
-                }
-            }
-
-            /**
-             * IML-LOADER
-             * ImlLoader loads all IML functions available within the app
-             * Following condition specifies where are IML functions allowed
-             */
-            if (
-                (name === "api" || name === "epoch" || name === "attach" || name === "detach")
-            ) {
-                // Remove existing ImlProvider
-                if (currentImlProvider !== null && currentImlProvider !== undefined) {
-                    currentImlProvider.dispose()
-                }
-
-                // IML functions list url
-                let url = `${_environment}/app/${app}/${version}/function`
-
-                // Get list of IML functions
-                let response = await Core.rpGet(url, _authorization)
-                let imls = response.map(iml => {
-                    return `${iml.name}()`
-                })
-
-                // Register a new ImlProvider
-                currentImlProvider = vscode.languages.registerCompletionItemProvider({
-                    scheme: 'file',
-                    language: 'imljson'
-                }, new ImlProvider(imls))
-            }
-            else {
-
-                // If out of scope, remove existing ImlProvider
-                if (currentImlProvider !== null && currentImlProvider !== undefined) {
-                    currentImlProvider.dispose()
-                }
-            }
-        })        
-        //#endregion
-
+        let coreCommands = new CoreCommands(appsProvider, _authorization, _environment, currentRpcProvider, currentImlProvider)
+        await CoreCommands.register(_DIR, _authorization, _environment)
         await AppCommands.register(appsProvider, _authorization, _environment)
         await ConnectionCommands.register(appsProvider, _authorization, _environment)
         await WebhookCommands.register(appsProvider, _authorization, _environment)
@@ -523,6 +192,11 @@ async function activate() {
         await CommonCommands.register(appsProvider, _authorization, _environment)
         await ChangesCommands.register(appsProvider, _authorization, _environment)
 
+        /**
+         * Registering events
+         */
+        vscode.workspace.onDidSaveTextDocument(editor => coreCommands.sourceUpload(editor))
+        vscode.window.onDidChangeActiveTextEditor(editor => coreCommands.keepProviders(editor))
     }
 
 }
