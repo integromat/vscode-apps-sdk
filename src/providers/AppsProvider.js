5b2cde4d18f6398626bdfcbf8ffdd697d069328b
Dominik Kadera <dominik.kadera@integromat.com>
2019-03-06 11:57:36 +0100

    Categories -> Groups

--- src/providers/AppsProvider.js
+++ src/providers/AppsProvider.js
@@ -115,7 +115,7 @@ class AppsProvider {
 			})
 
 			// Add Categories
-			output.push(new Code('categories', 'Categories', element, "jsonc", "app"));
+			output.push(new Code('groups', 'Groups', element, "json", "app"));
 			return output;
 		}
         /*
