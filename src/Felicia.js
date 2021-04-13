42b039fdc7a2b23cdd811471ba341271edbc8a32
Dominik Kadera <dominik.kadera@integromat.com>
2021-04-13 12:12:33 +0200

    clean tree

--- src/Felicia.js
+++ src/Felicia.js
@@ -1,85 +1,126 @@
 const vscode = require('vscode');
+const { IML } = require('@integromat/iml');
+const camelCase = require('lodash.camelcase');
 
 // Not a Formula, not a Forman, this is just a Felicia (from latin/italian "felice" = happy)
 // It will make you happy as it generates VSCode Forms for you based on Integromat Descriptive Syntax
 
 class ValidationError extends Error {
-	constructor(message) {
-		super(message);
-	}
+    constructor(message) {
+        super(message);
+    }
 }
 
 const types = {
-	text: async (field) => {
-		const validate = (spec, value) => {
-			if (spec.pattern !== undefined && !(RegExp(spec.pattern).test(value))) {
-				return `Value must match following pattern: /${spec.pattern}/.`
-			}
-			if (spec.min !== undefined && value.length < spec.min) {
-				return `Value must be at least ${spec.min} characters long.`
-			}
-			if (spec.max !== undefined && value.length > spec.max) {
-				return `Value must be at most ${spec.max} characters long`
-			}
-			return undefined;
-		}
+    select: async(field, context) => {
 
-		const value = await vscode.window.showInputBox({
-			prompt: field.label,
-			value: field.default,
-			validateInput: field.validate ? validate.bind(null, field.validate) : undefined
-		});
+        const nestedDictionary = field.options.reduce((dictionary, option) => {
+            if (option.nested) {
+                dictionary[option.value] = option.nested;
+            }
+            return dictionary;
+        }, {});
+        const options = field.options.map(option => {
+            return {
+                label: option.label,
+                description: option.value
+            }
+        });
 
-		if (field.required === true && (value === null || value === '')) {
-			throw new ValidationError(`Field ${field.name} is required.`);
-		}
+        // FIXME: THIS
 
-		return value;
-	},
-	number: async (field) => {
-		const validate = (spec, value) => {
-			try {
-				parseInt(value);
-			} catch (err) {
-				return err;
-			}
-			if (spec.min !== undefined && value < spec.min) {
-				return `Numeric value must be at least ${spec.min}.`
-			}
-			if (spec.max !== undefined && value > spec.max) {
-				return `Numeric value must be at most ${spec.max}.`
-			}
-			return undefined;
-		}
+        const value = await vscode.window.showQuickPick(options, {
+            placeHolder: field.label
+        });
 
-		const value = await vscode.window.showInputBox({
-			prompt: field.label,
-			value: field.default,
-			validateInput: validate.bind(null, field.validate || {})
-		});
+        if (nestedDictionary[value]) {
+            const nested = await module.exports(nestedDictionary[value]);
+            Object.entries(nested).forEach(([key, value]) => {
+                context[key] = value;
+            });
+        }
+        return value;
+    },
+    text: async(field, context) => {
+        const validate = (spec, value) => {
+            if (spec.pattern !== undefined && !(RegExp(spec.pattern).test(value))) {
+                return `Value must match following pattern: /${spec.pattern}/.`
+            }
+            if (spec.min !== undefined && value.length < spec.min) {
+                return `Value must be at least ${spec.min} characters long.`
+            }
+            if (spec.max !== undefined && value.length > spec.max) {
+                return `Value must be at most ${spec.max} characters long`
+            }
+            return undefined;
+        }
 
-		if (field.required === true && (value === undefined || value === null || value === '')) {
-			throw new ValidationError(`Field ${field.name} is required.`);
-		}
+        const value = await vscode.window.showInputBox({
+            prompt: field.label,
+            value: IML.execute(IML.parse(field.default), {
+                context
+            }, {
+                functions: {
+                    camelCase
+                }
+            }),
+            validateInput: field.validate ? validate.bind(null, field.validate) : undefined
+        });
 
-		return value;
-	}
+        if (field.required === true && (value === null || value === '')) {
+            throw new ValidationError(`Field ${field.name} is required.`);
+        }
+
+        return value;
+    },
+    number: async(field, context) => {
+        const validate = (spec, value) => {
+            try {
+                parseInt(value);
+            } catch (err) {
+                return err;
+            }
+            if (spec.min !== undefined && value < spec.min) {
+                return `Numeric value must be at least ${spec.min}.`
+            }
+            if (spec.max !== undefined && value > spec.max) {
+                return `Numeric value must be at most ${spec.max}.`
+            }
+            return undefined;
+        }
+
+        const value = await vscode.window.showInputBox({
+            prompt: field.label,
+            value: IML.execute(IML.parse(field.default), { context }, {
+                functions: {
+                    camelCase
+                }
+            }),
+            validateInput: validate.bind(null, field.validate || {})
+        });
+
+        if (field.required === true && (value === undefined || value === null || value === '')) {
+            throw new ValidationError(`Field ${field.name} is required.`);
+        }
+
+        return value;
+    }
 }
 
-module.exports = async (config) => {
-	try {
-		const data = {};
-		for (const field of config) {
-			if (types[field.type] !== undefined) {
-				data[field.name] = await types[field.type](field);
-				if (data[field.name] === undefined) {
-					return undefined;
-				}
-			}
-		}
-		return data;
-	} catch (err) {
-		vscode.window.showWarningMessage(err.message);
-		return undefined;
-	}
+module.exports = async(config) => {
+    try {
+        const data = {};
+        for (const field of config) {
+            if (types[field.type] !== undefined) {
+                data[field.name] = await types[field.type](field, data);
+                if (data[field.name] === undefined) {
+                    return undefined;
+                }
+            }
+        }
+        return data;
+    } catch (err) {
+        vscode.window.showWarningMessage(err.message);
+        return undefined;
+    }
 }
--- src/Validator.js
+++ src/Validator.js
@@ -1,23 +1,23 @@
 module.exports = {
-	functionName: function (string) {
-		return RegExp("(^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$)").test(string) ? undefined : "Function name must match the following RegExp: (^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$), for example: myFunction"
-	},
-	rpcName: function (string) {
-		return RegExp("(^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$)").test(string) ? undefined : "RPC id must match the following RegExp: (^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$), for example: myRpc"
-	},
-	moduleName: function (string) {
-		return RegExp("(^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$)").test(string) ? undefined : "Module id must match the following RegExp: (^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$), for example: myModule"
-	},
-	appName: function (string) {
-		return RegExp("(^[a-z][0-9a-z-]+[0-9a-z]$)").test(string) ? undefined : "App name must match the following RegExp: (^[a-z][0-9a-z-]+[0-9a-z]$), for example: my-new-app"
-	},
-	appTheme: function (string) {
-		return RegExp("^(#[0-9A-Fa-f]{6})$").test(string) ? undefined : "String is not valid color."
-	},
-	urlFormat: function (string) {
-		return RegExp("(integromat)(.)((com|cloud))").test(string) ? undefined : "URL is not valid Integromat API URL."
-	},
-	commitMessage: function (string) {
-		return (string.length <= 1000 && string.length > 0) ? undefined : "Commit message must have not be empty and must have at most 1000 characters."
-	}
+    functionName: function(string) {
+        return RegExp("(^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$)").test(string) ? undefined : "Function name must match the following RegExp: (^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$), for example: myFunction"
+    },
+    rpcName: function(string) {
+        return RegExp("(^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$)").test(string) ? undefined : "RPC id must match the following RegExp: (^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$), for example: myRpc"
+    },
+    moduleName: function(string) {
+        return RegExp("(^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$)").test(string) ? undefined : "Module id must match the following RegExp: (^[a-zA-Z][0-9a-zA-Z]+[0-9a-zA-Z]$), for example: myModule"
+    },
+    appName: function(string) {
+        return RegExp("(^[a-z][0-9a-z-]+[0-9a-z]$)").test(string) ? undefined : "App name must match the following RegExp: (^[a-z][0-9a-z-]+[0-9a-z]$), for example: my-new-app"
+    },
+    appTheme: function(string) {
+        return RegExp("^(#[0-9A-Fa-f]{6})$").test(string) ? undefined : "String is not valid color."
+    },
+    urlFormat: function(string) {
+        return RegExp("(integromat)(.)((com|cloud|local|dev))").test(string) ? undefined : "URL is not valid Integromat API URL."
+    },
+    commitMessage: function(string) {
+        return (string.length <= 1000 && string.length > 0) ? undefined : "Commit message must have not be empty and must have at most 1000 characters."
+    }
 }
