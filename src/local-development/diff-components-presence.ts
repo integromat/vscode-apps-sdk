c3e83961d3bd122aadc2e4e9fcb52a78317ac38d
Martin Zaloudek <ma-zal@users.noreply.github.com>
2023-08-21 13:41:04 +0200

    commited missing changes to "communition component create"

--- src/local-development/diff-components-presence.ts
+++ src/local-development/diff-components-presence.ts
@@ -10,23 +10,30 @@ export function diffComponentsPresence(
 	reference: AppComponentTypesMetadata<AppComponentMetadata>,
 	skipCheckMissing = false,
 ): {
-	newComponents: { componentType: AppComponentType; componentName: string }[];
-	missingComponents: { componentType: AppComponentType; componentName: string }[];
+	newComponents: {
+		componentType: AppComponentType;
+		componentName: string;
+		componentMetadata: AppComponentMetadata;
+	}[];
+	missingComponents: {
+		componentType: AppComponentType;
+		componentName: string;
+		componentMetadata: AppComponentMetadata;
+	}[];
 } {
 	const ret: ReturnType<typeof diffComponentsPresence> = {
 		newComponents: [],
-		missingComponents: skipCheckMissing
-			? []
-			: diffComponentsPresence(reference, allComponents, true).newComponents,
+		missingComponents: skipCheckMissing ? [] : diffComponentsPresence(reference, allComponents, true).newComponents,
 	};
 
 	for (const [componentType, components] of Object.entries(allComponents)) {
-		for (const componentName of Object.keys(components)) {
+		for (const [componentName, componentMetadata] of Object.entries(components)) {
 			const referenceComponentMetadata = reference[componentType as AppComponentType]?.[componentName];
 			if (referenceComponentMetadata === undefined) {
 				ret.newComponents.push({
 					componentType: componentType as AppComponentType,
 					componentName,
+					componentMetadata,
 				});
 			}
 		}
