c68ed74995e1dd76e855794198b79d1665549b86
Martin Zaloudek <ma-zal@users.noreply.github.com>
2023-10-06 12:33:55 +0200

    test: language ID is correctly used

--- src/extension.test.ts
+++ src/extension.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'node:assert';
-import { suite } from 'mocha';
+import { after, suite } from 'mocha';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
@@ -19,7 +19,7 @@ suite('Extension Intialization Tests', () => {
 		assert.ok(vscode.extensions.getExtension('Integromat.apps-sdk')?.isActive);
 	}).timeout(5000);
 
-	test('IMLJSON language exists', async() => {
+	test('IMLJSON language exists', async () => {
 		const languages = await vscode.languages.getLanguages();
 		assert.ok(languages.includes('imljson'), `Languages are only: ${languages.join(', ')}`);
 	});
@@ -28,5 +28,41 @@ suite('Extension Intialization Tests', () => {
 		const imljsonLanguageClient = testsOnly_getImljsonLanguageClient();
 		assert.equal(imljsonLanguageClient?.name, 'IMLJSON language server', 'Unexpected IMLJSON language server name');
 		assert.equal(imljsonLanguageClient?.isRunning(), true);
+	});
+});
+
+/**
+ * See https://stackoverflow.com/questions/38279920/how-to-open-file-and-insert-text-using-the-vscode-api
+ * how to open file
+ */
+suite('Language ID tests for files used by online edits', () => {
+	const filenamesForOnlineEdit = [
+		{ filename: 'parameters.imljson', language: 'imljson' },
+		{ filename: 'expect.imljson', language: 'imljson' },
+		{ filename: 'interface.imljson', language: 'imljson' },
+		{ filename: 'common.imljson', language: 'imljson' },
+		{ filename: 'api.imljson', language: 'imljson' },
+		{ filename: 'samples.imljson', language: 'imljson' },
+		{ filename: 'scopes.imljson', language: 'imljson' },
+		{ filename: 'scope.imljson', language: 'imljson' },
+		{ filename: 'epoch.imljson', language: 'imljson' },
+		{ filename: 'attach.imljson', language: 'imljson' },
+		{ filename: 'detach.imljson', language: 'imljson' },
+		{ filename: 'publish.imljson', language: 'imljson' },
+		{ filename: 'base.imljson', language: 'imljson' },
+		{ filename: 'api-oauth.imljson', language: 'imljson' },
+		{ filename: 'groups.json', language: 'json' },
+	];
+	for (const def of filenamesForOnlineEdit) {
+		test(`File ${def.filename} language should be ${def.language.toUpperCase()}`, async () => {
+			const textDocument = await vscode.workspace.openTextDocument(vscode.Uri.parse('untitled:' + def.filename));
+			// const e = await vscode.window.showTextDocument(a, 1, false);
+			// e.edit((edit) => { edit.insert(new vscode.Position(0, 0), '{}'); });
+			assert.equal(textDocument.languageId, def.language, 'Language ID comparision');
+		});
+	}
+
+	after(async () => {
+		await vscode.commands.executeCommand('workbench.action.closeAllEditors');
 	});
 });
