511fbf010252f7600aba9cae1bba96af5ada47dc
Sourcery AI <>
2023-11-15 11:07:35 +0000

    'Refactored by Sourcery'

--- resources/icons/light-to-dark.py
+++ resources/icons/light-to-dark.py
@@ -5,7 +5,7 @@ import PIL.ImageOps
 for file in os.listdir("light"):
     if file.endswith(".png"):
         path = os.path.join("light", file)
-        print("[ LOG ] Loading file " + path)
+        print(f"[ LOG ] Loading file {path}")
         image = Image.open(path)
         r,g,b,a = image.split()
         rgb_image = Image.merge('RGB', (r,g,b))
@@ -13,6 +13,6 @@ for file in os.listdir("light"):
         r2,g2,b2 = inverted_image.split()
         final_transparent_image = Image.merge('RGBA', (r2,g2,b2,a))
         new_path = os.path.join("dark", file)
-        print("[ LOG ] Saving file to " + new_path)
+        print(f"[ LOG ] Saving file to {new_path}")
         final_transparent_image.save(new_path)
 print("Done")
