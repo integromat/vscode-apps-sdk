65cf55948a005875de3d8d34123ef18ad7ba9dab
Martin Zaloudek <ma-zal@users.noreply.github.com>
2023-12-14 14:29:20 +0100

    refactor: getJsonSchemas

--- src/LanguageServersSettings.ts
+++ src/LanguageServersSettings.ts
@@ -1,47 +1,51 @@
 import * as path from 'node:path';
 import * as vscode from 'vscode';
+import { SchemaConfiguration } from 'vscode-json-languageservice';
 import * as vscodeLanguageclient from 'vscode-languageclient/node';
-
-// Type copied from `jsonServer.ts, which is the receiver of this this datatype.
-type ISchemaAssociations = Record<string, string[]>;
 
 /**
  * Gets all JSON schemas definitions specified in this extensions
- * in the `package.json` -> `contributes` -> `jsonValidation`.
- *
- * TODO Convert return into `SchemaConfiguration[]` from 'vscode-json-languageservice'
+ * in the `package.json` -> `contributes` -> `jsonValidation`
+ * and converts it into `SchemaConfiguration[]` datatype.
  */
-export function getJsonSchemas(): ISchemaAssociations {
-	const associations: ReturnType<typeof getJsonSchemas> = {};
-	const extension = vscode.extensions.getExtension('Integromat.apps-sdk');
-	const jsonValidation = extension?.packageJSON?.contributes?.jsonValidation;
-	if (extension && Array.isArray(jsonValidation)) {
-		jsonValidation.forEach(({ fileMatch, url }) => {
-			const fileMatches: string[] = Array.isArray(fileMatch) ? fileMatch : [fileMatch];
-			for (let fileMatch of fileMatches) {
-				if (url[0] === '.' && url[1] === '/') {
-					url = vscode.Uri.file(path.join(extension.extensionPath, url)).toString();
-				}
-				if (fileMatch[0] === '%') {
-					fileMatch = fileMatch.replace(/%APP_SETTINGS_HOME%/, '/User');
-					fileMatch = fileMatch.replace(/%APP_WORKSPACES_HOME%/, '/Workspaces');
-				} else if (
-					typeof fileMatch === 'string' &&
-					fileMatch.charAt(0) !== '/' &&
-					!fileMatch.match(/\w+:\/\//)
-				) {
-					fileMatch = '/' + fileMatch;  // TODO Is it needed to add "/"?
-				}
-				let association = associations[fileMatch];
-				if (!association) {
-					association = [];
-					associations[fileMatch] = association;
-				}
-				association.push(url);
-			}
-		});
+export function getJsonSchemas(): SchemaConfiguration[] {
+	const extension = vscode.extensions.getExtension('Integromat.apps-sdk')!;
+
+	const jsonValidation: { fileMatch: string | string[]; url: string}[] =
+		extension?.packageJSON?.contributes?.jsonValidation ?? [];
+	if (!Array.isArray(jsonValidation)) {
+		throw new Error('package.json -> contributes -> jsonValidation must be an array.');
 	}
-	return associations;
+
+	return jsonValidation.map((jsonValidationItem) => {
+		const originalFileMatches: string[] = Array.isArray(jsonValidationItem.fileMatch)
+			? jsonValidationItem.fileMatch
+			: [jsonValidationItem.fileMatch];
+
+		let uri = jsonValidationItem.url;
+		if (uri.startsWith('./')) {
+			uri = vscode.Uri.file(path.join(extension.extensionPath, uri)).toString();
+		}
+
+		const finalFileMatches = originalFileMatches.map((fileMatch) => {
+			if (fileMatch.startsWith('%')) {
+				fileMatch = fileMatch.replace(/%APP_SETTINGS_HOME%/, '/User');
+				fileMatch = fileMatch.replace(/%APP_WORKSPACES_HOME%/, '/Workspaces');
+			} else if (
+				typeof fileMatch === 'string' &&
+				fileMatch.startsWith('/') &&
+				!(/\w+:\/\//.test(fileMatch))
+			) {
+				fileMatch = '/' + fileMatch;  // TODO Is it needed to add "/"?
+			}
+			return fileMatch;
+		});
+
+		return {
+			uri,
+			fileMatch: finalFileMatches
+		};
+	});
 }
 
 export const clientOptions = {
