6d676e4b2404112a678dff8a370678abb26e2ebd
Martin Zaloudek <ma-zal@users.noreply.github.com>
2024-01-03 13:05:27 +0100

    feat: new flag isPreReleaseVersion

--- src/Meta.ts
+++ src/Meta.ts
@@ -4,3 +4,18 @@ import * as vscode from 'vscode';
  * The version of the currently running VSCode Extension
  */
 export const version: string = vscode.extensions.getExtension('Integromat.apps-sdk')!.packageJSON.version;
+
+// Production -> load from file created during the publishing process.
+// See `package.json` -> `scripts` for details.
+let _isPreReleaseVersion: boolean = false;
+try {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	_isPreReleaseVersion = require('./__is-prerelease-version').isPreReleaseVersion;
+} catch (e: any) {
+	/* ignore */
+}
+
+export const isPreReleaseVersion: boolean =
+	// - Always `true` in local debug run,
+	// - Load from flag file in production.
+	vscode.extensions.getExtension('Integromat.apps-sdk')!.packageJSON.isUnderDevelopment || _isPreReleaseVersion;
