6fbda023fc31a0730787274c4860c5c95dd4b3e6
Martin Zaloudek <ma-zal@users.noreply.github.com>
2024-05-22 13:10:16 +0200

    fix texts

--- src/local-development/clone.ts
+++ src/local-development/clone.ts
@@ -76,7 +76,7 @@ async function cloneAppToWorkspace(context: App): Promise<void> {
 				'\n\n' +
 				'Common data could contain sensitive data or secrets. It depends on your app design. ' +
 				'We recommend that you exclude common data files from the local clone of the application. ' +
-				'If you are decide to include it, be aware that these common data files will be also the part of your GIT commits.',
+				'If you decide to include it, be aware that these common data files will also be part of your GIT commits.',
 		},
 		{ title: 'Exclude (more safe)' },
 		{ title: 'Include (for advanced users only)' },
