fa05625c64ad61adb77ae96b29aa33c3f8908a3c
Michal Fuka <m.fuka@make.com>
2024-07-22 15:34:59 +0200

    feat: ability to debug extension from anywhere (#185)

---
+++ debug.sh
@@ -0,0 +1,9 @@
+# Setup
+# 1. Add 'code' to $PATH
+# 2. chmod +x debug.sh
+
+# Compile code
+npm run compile
+
+# Run Visual Code with compiled extension (no installation needed)
+code --disable-extensions --extensionDevelopmentPath=$(pwd)
